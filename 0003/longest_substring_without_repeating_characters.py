d5388eb9e3163f41ec737ef76af4051bab467bb1
NonymousMorlock <agamspaul@gmail.com>
2024-11-30 23:28:58 +0000

    🗑 Chore: Solve Longest Substring without repeating characters

---
+++ 0003/longest_substring_without_repeating_characters.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+def length_of_longest_substring(original_string: str) -> int:
+    length = 0
+    # Edge-cases: Empty string - Taken care of by the loop since the loop won't run at all if the string is empty
+
+    visited = None
+
+    for firstPointer in range(len(original_string)):
+        visited = set()
+        for secondPointer in range(firstPointer, len(original_string)):
+            if original_string[secondPointer] in visited:
+                if len(visited) > length:
+                    length = len(visited)
+                break
+            visited.add(original_string[secondPointer])
+        if len(visited) > length:
+            length = len(visited)
+
+    return length
