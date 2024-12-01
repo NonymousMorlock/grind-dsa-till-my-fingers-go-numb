796a9f4c126cde98e919c9c64b043a3b7a739b70
NonymousMorlock <agamspaul@gmail.com>
2024-12-01 20:37:01 +0000

    🗑 Chore: Add note for the time complexity of day 0003 solution

--- 0003/longest_substring_without_repeating_characters.py
+++ 0003/longest_substring_without_repeating_characters.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 
+#  O(n²)
 def length_of_longest_substring(original_string: str) -> int:
     length = 0
     # Edge-cases: Empty string - Taken care of by the loop since the loop won't run at all if the string is empty
