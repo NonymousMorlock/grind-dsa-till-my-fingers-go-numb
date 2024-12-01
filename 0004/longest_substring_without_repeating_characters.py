2b0fd6562504f58402a624aff368e50b8d3943ec
NonymousMorlock <agamspaul@gmail.com>
2024-12-01 20:38:04 +0000

    🗑 Chore: Solve Longest Substring without repeating characters in `O(n)`
    - Used sliding window and hash set

---
+++ 0004/longest_substring_without_repeating_characters.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+# O(n) - Sliding window and hash set
+def length_of_longest_substring_1(original_string: str) -> int:
+    left_pointer = max_length = 0
+
+    character_set = set()
+
+    for right_pointer in range(len(original_string)):
+        while original_string[right_pointer] in character_set:
+            character_set.remove(original_string[left_pointer])
+
+        character_set.add(original_string[right_pointer])
+        max_length = max(max_length, right_pointer - left_pointer + 1)
+
+    return max_length
