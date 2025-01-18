c2f476fbf4132d3b0db29215026aef47218afd9f
NonymousMorlock <agamspaul@gmail.com>
2025-01-18 21:34:32 +0000

    🗑 Chore: Solve "Climbing Stairs"[DP Space Optimized approach]
    - space complexity: O(1)
    - time complexity: O(n)

--- 0009/climbing_stairs.py
+++ 0009/climbing_stairs.py
@@ -1,16 +1,10 @@
 class Solution:
     def climb_stairs(self, n: int) -> int:
-        memo = {}
-        def recursor(current_value: int):
-            # since we are always going -2 at most, 2-2 will land us at 0, when we get to 1 we return 1 so the max depth
-            # when it's a binary tree will be 2, otherwise we'll have to consider negatives
-            if current_value < 2: return 1
+        left_pointer = right_pointer = 1
 
-            if current_value in memo:
-                return memo[current_value]
+        for _ in range(n - 1):
+            cache = left_pointer
+            left_pointer = left_pointer + right_pointer
+            right_pointer = cache
 
-            memo[current_value] = recursor(current_value - 1) + recursor(current_value - 2)
-
-            return memo[current_value]
-
-        return recursor(n)
+        return left_pointer
