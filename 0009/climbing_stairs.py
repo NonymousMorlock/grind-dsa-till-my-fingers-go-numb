a9a799eb598102cc7cc52b7d7405afe1e95a64b1
NonymousMorlock <agamspaul@gmail.com>
2025-01-18 21:31:27 +0000

    🗑 Chore: Solve "Climbing Stairs"[bottom-up approach]

--- 0009/climbing_stairs.py
+++ 0009/climbing_stairs.py
@@ -1,14 +1,15 @@
 class Solution:
     def climb_stairs(self, n: int) -> int:
         memo = {}
-        def recursor(target: int, current_value: int = 0):
-            if current_value == target: return 1
-            elif current_value > target: return 0
+        def recursor(current_value: int):
+            # since we are always going -2 at most, 2-2 will land us at 0, when we get to 1 we return 1 so the max depth
+            # when it's a binary tree will be 2, otherwise we'll have to consider negatives
+            if current_value < 2: return 1
 
             if current_value in memo:
                 return memo[current_value]
 
-            memo[current_value] = recursor(target, current_value + 1) + recursor(target, current_value + 2)
+            memo[current_value] = recursor(current_value - 1) + recursor(current_value - 2)
 
             return memo[current_value]
 
